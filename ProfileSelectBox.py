a250c21c86737655b5d9a2298aeaffcf3c6d0e18
bw555 <ttrui92aa@gmail.com>
2023-01-15 13:59:17 -0800

    Make UI clickable

--- ProfileSelectBox.py
+++ ProfileSelectBox.py
@@ -1,9 +1,10 @@
 import tkinter as tk
+from tkinter import ttk
 from LoginData import LoginData
 from CustomExceptions import InvalidProfileException
 from LoginDataValidator import get_requested_accounts
 
-DEFAULT_BUTTON_TEXT = 'No valid profile entered'
+DEFAULT_BUTTON_TEXT = 'No profile or character selected.'
 
 
 class ProfileSelectBox:
@@ -11,27 +12,49 @@ class ProfileSelectBox:
         self.profiles = profiles
         self.selected_profile: None | list[LoginData] = None
         self.char_names = char_names
+        self.selection = ''
+        name_counts = [len(x) for x in profiles.values()]
+        max_count = max(name_counts)
 
         self.window = tk.Tk()
-        self.window.title('Profile Selection')
-        self.window.option_add('*font', 'consolas 12')
-        tk.Label(self.window, text='Windower Profile').grid(row=0, column=0)
-        tk.Label(self.window, text='Characters').grid(row=0, column=1)
+        self.window.title('Profile/Character Selection')
+        self.window.option_add('*font', 'consolas 12 bold')
+        self.window.configure(bg='black')
+        tk.Label(
+            self.window,
+            text='Windower Profile',
+            background='green',
+            foreground='white',
+        ).grid(row=0, column=0, ipadx=10, padx=30, ipady=10, pady=5)
+        tk.Label(
+            self.window, text='Characters', background='green', foreground='white'
+        ).grid(row=0, column=1, columnspan=max_count, ipadx=50, ipady=10, pady=5)
         for i, (profile, accounts) in enumerate(self.profiles.items()):
-            tk.Label(self.window, text=f'{profile:>}').grid(row=i + 1, column=0)
+            tk.Button(
+                self.window,
+                text=f'{profile:>}'.capitalize(),
+                command=lambda profile=profile: self.check_profile_validity_button(
+                    profile
+                ),
+                background='#005C99',
+                cursor='hand2',
+                foreground='white',
+                borderwidth=5,
+            ).grid(row=i + 1, column=0, padx=20)
             names = [account.character_name for account in accounts]
-            tk.Label(self.window, text=f'{", ".join(names):<}').grid(
-                row=i + 1, column=1
+            name_buttons = ttk.Frame(self.window).grid(
+                row=i + 1, column=1, columnspan=max_count
             )
-
-        self.instruction = tk.Label(
-            text='Enter profile name (or single character name):'
-        )
-        self.instruction.grid(row=len(self.profiles) + 1, column=0)
-
-        self.profile_entry = tk.Entry(master=self.window)
-        self.profile_entry.grid(row=len(self.profiles) + 1, column=1)
-        self.profile_entry.bind('<KeyRelease>', self.check_profile_validity)
+            for col, name in enumerate(names):
+                button = tk.Button(
+                    name_buttons,
+                    text=f'{name.capitalize()}',
+                    command=lambda name=name: self.check_profile_validity_button(name),
+                    background='#005C99',
+                    cursor='hand2',
+                    foreground='white',
+                    borderwidth=5,
+                ).grid(row=i + 1, column=col + 1, columnspan=1, pady=25)
 
         self.confirm_button = tk.Button(
             self.window,
@@ -39,31 +62,33 @@ class ProfileSelectBox:
             fg='white',
             state=tk.DISABLED,
             command=self.login_selected,
+            borderwidth=5,
         )
-        self.confirm_button.grid(columnspan=2)
+        self.confirm_button.grid(columnspan=max_count + 1, padx=10, pady=10)
 
         self.window.mainloop()
 
-    def check_profile_validity(self, event) -> None:
-        if self.profile_entry.get() in self.profiles:
-            names = [
-                account.character_name
-                for account in self.profiles.get(self.profile_entry.get())
-            ]
+    def check_profile_validity_button(self, name) -> None:
+        if name.lower() in {profile.lower() for profile in self.profiles}:
+            names = [account.character_name for account in self.profiles.get(name)]
             self.confirm_button['state'] = tk.NORMAL
             self.confirm_button['text'] = f'Click here to login on {", ".join(names)}'
+            self.confirm_button['cursor'] = 'hand2'
             self.confirm_button['bg'] = 'green'
-        elif self.profile_entry.get() in self.char_names:
+            self.selection = name
+        elif name.lower() in {name.lower() for name in self.char_names}:
             self.confirm_button['state'] = tk.NORMAL
-            self.confirm_button[
-                'text'
-            ] = f'Click here to login on {self.profile_entry.get()}'
+            self.confirm_button['text'] = f'Click here to login on {name.capitalize()}'
+            self.confirm_button['cursor'] = 'hand2'
             self.confirm_button['bg'] = 'green'
+            self.selection = name
         else:
             self.confirm_button['state'] = tk.DISABLED
             self.confirm_button['text'] = DEFAULT_BUTTON_TEXT
             self.confirm_button['bg'] = '#f0f0f0'
+            self.confirm_button['cursor'] = 'none'
+            self.selection = ''
 
     def login_selected(self) -> str:
-        self.selected_profile = get_requested_accounts(self.profile_entry.get())
+        self.selected_profile = get_requested_accounts(self.selection)
         self.window.destroy()
